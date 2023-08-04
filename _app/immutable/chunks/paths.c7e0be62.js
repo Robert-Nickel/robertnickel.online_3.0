ea9ef95665a53bccba97109c19a48f3f7655000c
Robert Nickel <github@robertnickel.online>
2023-08-04 13:37:58 +0200

    Updates

--- _app/immutable/chunks/paths.c7e0be62.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_1rgye0v)==null?void 0:s.base)??"/robertnickel.online_3.0";var e;const a=((e=globalThis.__sveltekit_1rgye0v)==null?void 0:e.assets)??t;export{a,t as b};
---
+++ _app/immutable/chunks/paths.d2e52f99.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_14v86om)==null?void 0:s.base)??"/robertnickel.online_3.0";var e;const a=((e=globalThis.__sveltekit_14v86om)==null?void 0:e.assets)??t;export{a,t as b};
