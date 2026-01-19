eea89af8f0650692a93dbea348c623f83da24a4f
Robert Nickel <github@robertnickel.online>
2026-01-19 15:11:15 +0100

    updated current activities

--- src/routes/now/+page.svelte
+++ src/routes/now/+page.svelte
@@ -2,11 +2,6 @@
     <h1>Now</h1>
     <p>- working at <a href="https://www.seitenbau.com/">SEITENBAU</a></p>
     <p>
-        - training for my first <a href="https://www.wuerzburg-marathon.de/"
-            >marathon</a
-        > on May 25th, 2025
-    </p>
-    <p>
         - studying <a href="https://www.haufe-akademie.de/34218"
             >Systemic Business Coaching</a
         > at Haufe Academy
