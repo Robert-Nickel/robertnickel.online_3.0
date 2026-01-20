d9ca5a14cfdbbbf95feab70671cf07e1a9430b2b
Robert Nickel <github@robertnickel.online>
2026-01-20 09:19:54 +0100

    Added Whereby Integration

---
+++ src/routes/coaching/+page.svelte
@@ -0,0 +1,4 @@
+<div style="margin: auto;">
+    <h1>Coaching</h1>
+    <iframe src="https://robertnickel.whereby.com/coachingc8e005b9-46c7-46d3-b269-e47771f8d2bf" allow="camera; microphone; fullscreen; speaker; display-capture; compute-pressure" style="height: 700px; width: 100%"></iframe>
+</div>
